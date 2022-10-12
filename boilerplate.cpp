08ef1e4a378d17632111a10dae15ca719190bce1
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-10-12 16:55:56 -0400

    update boilerplate.cpp

--- boilerplate.cpp
+++ boilerplate.cpp
@@ -8,9 +8,6 @@ using ull = unsigned long long;
 #define repeat(_i, _x) for(int _i = 0; _i < (_x); ++_i)
 #define for_range(_i, _start, _end, _itr) for (int _i = (_start); i < (_end); _i += _itr)
 
-#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
-#define TRAV(a,x) for (auto& a: x)
-
 void setIO(const string str = "") {
     ios::sync_with_stdio(false);
 #ifdef LOCAL // compile with -DLOCAL
@@ -31,7 +28,7 @@ int main() {
     
     vector<int> v(len);
     
-    FOR(i,0,len) {
+    repeat(i, len) {
         cin >> v[i];
     }
 }
