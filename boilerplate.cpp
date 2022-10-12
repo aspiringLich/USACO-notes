535c79d45254fd7ec0b14be56b02bf06a3bc487d
aspiringLich <48413902+aspiringLich@users.noreply.github.com>
2022-10-12 16:55:05 -0400

    boilerplate

---
+++ boilerplate.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+using ull = unsigned long long;
+
+#define repeat(_i, _x) for(int _i = 0; _i < (_x); ++_i)
+#define for_range(_i, _start, _end, _itr) for (int _i = (_start); i < (_end); _i += _itr)
+
+#define FOR(i,a,b) for (int i = (a); i < (b); ++i)
+#define TRAV(a,x) for (auto& a: x)
+
+void setIO(const string str = "") {
+    ios::sync_with_stdio(false);
+#ifdef LOCAL // compile with -DLOCAL
+    freopen("test.in","r",stdin);
+#else
+    if (!str.empty()) { 
+        freopen((str + ".in").c_str(),"r",stdin);
+        freopen((str + ".out").c_str(),"w",stdout);
+    }
+#endif
+}
+
+int main() {
+    setIO();
+    
+    int len;
+    cin >> len; 
+    
+    vector<int> v(len);
+    
+    FOR(i,0,len) {
+        cin >> v[i];
+    }
+}
