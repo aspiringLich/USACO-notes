d046221e7400cd6599faa35304393419d8167cb4
aspiringLich <sirbrandonthenerd@gmail.com>
2022-12-12 16:19:50 -0500

    fix error

--- workspace/main.cpp
+++ workspace/main.cpp
@@ -8,11 +8,11 @@ using ull = unsigned long long;
 #define repeat(_i, _x) for(int _i = 0; _i < (_x); ++_i)
 #define for_range(_i, _start, _end, _itr) for (int _i = (_start); i < (_end); _i += _itr)
 
-void setIO(const string str = "", const bool flag = true) {
+void setIO(const string str = "") {
     ios::sync_with_stdio(false);
 #ifdef LOCAL // compile with -DLOCAL
     cout << "compiled locally" << endl;
-    if (str.empty() && flag) freopen("test.in", "r", stdin);
+    if (str.empty()) freopen("test.in", "r", stdin);
     else {
         freopen((str + ".in").c_str(), "r", stdin);
     }
